e04847f3cbfbb990523b1fa295141153a981d846
Ben Kluger <69221395+BenKluger@users.noreply.github.com>
2021-11-01 22:53:18 -0400

    Completed entire Instructor section

--- js/generalScripts.js
+++ js/generalScripts.js
@@ -1,3 +1,4 @@
+///////////////////////////TAB JS///////////////////////////
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -18,3 +19,13 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+//////////////////////////////SLIDER JS//////////////////////////////
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
